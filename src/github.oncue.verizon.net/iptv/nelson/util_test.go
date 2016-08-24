d32d309e7ca9a035de3e1fbe1f87cc09ee3b9f0e
Andrew Mohrland <andrew.mohrland@one.verizon.com>
2016-08-24 12:47:18 -0700

    remove version hack

--- src/github.oncue.verizon.net/iptv/nelson/util_test.go
+++ src/github.oncue.verizon.net/iptv/nelson/util_test.go
@@ -9,7 +9,7 @@ import (
 func TestUserAgentString(t *testing.T) {
   // Test for development, where global build version doesn't exist.
   var globalBuildVersion1 = ""
-  var expectedDevelUserAgentString1 = "NelsonCLI/10000.0.0"
+  var expectedDevelUserAgentString1 = "NelsonCLI/devel"
   var result1 = UserAgentString(globalBuildVersion1)
   if !strings.Contains(result1, expectedDevelUserAgentString1 + " (") {
     t.Error("devel user agent string is incorrect: \n" + result1 + "\n" + expectedDevelUserAgentString1)
