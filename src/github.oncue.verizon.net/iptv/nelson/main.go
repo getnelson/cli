5aa40c06c4c27dab382d3d86f144af49c7732c1d
Timothy Perrett <timothy.m.perrett@one.verizon.com>
2017-02-06 14:28:15 -0800

    change parlance

--- src/github.oncue.verizon.net/iptv/nelson/main.go
+++ src/github.oncue.verizon.net/iptv/nelson/main.go
@@ -457,7 +457,7 @@ func main() {
               req := ManualDeploymentRequest{
                 Datacenter: selectedDatacenter,
                 Namespace: selectedNamespace,
-                ServiceType: selectedServiceType,
+                UnitType: selectedServiceType,
                 Version: selectedVersion,
                 Hash: stackHash,
                 Description: description,
@@ -472,7 +472,7 @@ func main() {
                 fmt.Println(res)
               }
             } else {
-              return cli.NewExitError("You must specify the following switches: \n\t--datacenter <string> \n\t--namespace <string> \n\t--service-type <string> \n\t--version <string> \n\t--hash <string> \n\t--description <string> \n\t--port <int>", 1)
+              return cli.NewExitError("You must specify the following switches: \n\t--datacenter <string> \n\t--namespace <string> \n\t--service-type <string> \n\t--version <string> \n\t--hash <string> \n\t--port <int>", 1)
             }
             return nil
           },
