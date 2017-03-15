5a93f60e9520b0ef347399cf444c196302973393
Alice Wu <alice.wu@one.verizon.com>
2017-03-15 14:29:10 -0700

    change error message in case environment variable GITHUB_TOKEN is not set

--- src/github.oncue.verizon.net/iptv/nelson/main.go
+++ src/github.oncue.verizon.net/iptv/nelson/main.go
@@ -85,7 +85,7 @@ func main() {
 				}
 
 				if len(userGithubToken) <= 0 {
-					return cli.NewExitError("You must specifiy a `--token` or a `-t` to login.", 1)
+					return cli.NewExitError("You must specifiy a `--token` or a `-t` to login, or set your GITHUB_TOKEN environment variable", 1)
 				}
 
 				// fmt.Println("token: ", userGithubToken)
